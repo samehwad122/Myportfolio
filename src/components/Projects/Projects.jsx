25de106b8a46556615e41ac944ff5a4f935f146b
samehwad122 <samehwad2@gmail.com>
2025-07-29 02:34:08 +0300

    first commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,8 @@ import { motion } from 'framer-motion';
 import pro1 from "../../assets/images/img-pro/darkStore.png";
 import pro2 from "../../assets/images/img-pro/pasket.png";
 import pro3 from "../../assets/images/img-pro/papa.png";
+import pro4 from "../../assets/images/img-pro/fresh.png";
+import pro5 from "../../assets/images/img-pro/pro-css.png";
 
 function Projects() {
   return (
@@ -86,8 +88,53 @@ function Projects() {
             Papa John's restaurant project designed using React and API using supabase
           </p>
         </motion.div>
+        <motion.div
+          className="col-12 col-sm-8 col-md-5 col-lg-3 project-card"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.1 }}
+          viewport={{ once: true }}
+        >
+          <a
+            href="https://samehwad122.github.io/project-js/"
+            className="project-image-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="image-container">
+              <img src={pro4} alt="Pasket Project" className="project-image" />
+              <div className="overlay">View</div>
+            </div>
+          </a>
+          <h3 className="project-title glow-text">fresh meal store</h3>
+          <p className="project-description glow-text">
+            Fruit and vegetable store designed using html, css and javascript
+          </p>
+        </motion.div>
+        <motion.div
+          className="col-12 col-sm-8 col-md-5 col-lg-3 project-card"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.1 }}
+          viewport={{ once: true }}
+        >
+          <a
+            href="https://samehwad122.github.io/project-css/"
+            className="project-image-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="image-container">
+              <img src={pro5} alt="Pasket Project" className="project-image" />
+              <div className="overlay">View</div>
+            </div>
+          </a>
+          <h3 className="project-title glow-text">Games store</h3>
+          <p className="project-description glow-text">
+            Design an online store to sell games using HTML and CSS
+          </p>
+        </motion.div>
 
-        {/* يمكنك تكرار نفس الشكل لأي مشروع آخر */}
 
       </div>
     </div>
